274abc19ffab2214c5e93612dadebef92253c149
sppowel <sppowel@LaptoptimusPrime.castlebranch.com>
2018-03-22 06:30:02 -0400

    updated lambda func

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,16 +4,27 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
-    
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:928252719086:deployPortfolio')
+    location = {
+        "bucketName": 'portfolio.stevenpowell.xyz',
+        "objectKey": 'portfoliobuild.zip'
+    }
     try:
+        job = event.get("CodePipeline.job")
+        
+        if job:
+            for artifact in job["data"]["inputArtifacts"]:
+                if artifact["name"] == "MyAppBuild":
+                    location = artifact["location"]["s3Location"]
+                
+        print "building portfolio from " + str(location)
         s3 = boto3.resource('s3')
-        sns = boto3.resource('sns')
-        topic = sns.Topic('arn:aws:sns:us-east-1:928252719086:deployPortfolio')
         portfolio_bucket = s3.Bucket('portfolio.stevenpowell.xyz')
-        build_bucket = s3.Bucket('portfoliobuild.stevenpowell.xyz')
+        build_bucket = s3.Bucket(location["bucketName"])
         
         portfolio_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location["objectKey"], portfolio_zip)
     
         with zipfile.ZipFile(portfolio_zip) as myzip:
             for nm in myzip.namelist():
@@ -23,6 +34,9 @@ def lambda_handler(event, context):
             
         print "Mission Complete"
         topic.publish(Subject="portfolio deployed", Message="Mission Comprete. You are winrar!")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job["id"])
     except:
         topic.publish(Subject="portfolio not deployed", Message="Fission Mailed")
     return 'Hello from Lambda'
